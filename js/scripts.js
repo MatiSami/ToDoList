9ad2b0ff0ed2df415051fa92dcc6e77aeaa8bfbf
MatiSami <mateusz.samula@gmail.com>
2020-03-28 22:03:51 +0100

    styling app

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-const input = document.getElementById("input-task");
 const category = document.getElementById("category");
-
 const list = document.querySelector(".task-list");
 const button = document.getElementById("add-element");
-// const remove = document.getElementById("#remove")
-
-console.log(list);
-console.log(button);
 
 button.addEventListener("click", function() {
   let option = category.options[category.selectedIndex].value;
-  console.log(input.value, option, option.value);
+  const input = document.getElementById("input-task");
+
 
   var textnode = `<li class="item">
   <p>${input.value}</p>
   <div class="remove" onclick="remove(this)">
-  <i class="fa fa-trash">usuń</i>
- 
+  <i class="fa fa-trash"></i>
   </div>
-</li>`;
+  </li>`;
 
-  var node = document.createElement("LI"); // Create a <li> node
-  //   var textnode = input.value; // Create a text node
-  node.innerHTML = textnode; // Append the text to <li>
-  document.getElementById(option).appendChild(node); // Append <li> to <ul> with id="myList"
+  if (input.value.length > 0) {
+    var node = document.createElement("LI");
+    node.innerHTML = textnode;
+    document.getElementById(option).appendChild(node);
+    input.placeholder = "Wpisz zadanie do  wykonania...";
+    input.value = "";
+
+    input.classList.remove("input-task--error");
+  } else {
+    console.log(input);
+    input.classList.add("input-task--error");
+    input.placeholder = "Musisz coś wpisać";
+  }
 });
 
 function remove(link) {
