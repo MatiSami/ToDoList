3f6be3edec952bf789e717e1abd45f53a95ab650
MatiSami <mateusz.samula@gmail.com>
2020-04-02 14:47:04 +0200

    add Gulp to project

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,31 @@ const category = document.getElementById("category");
 const list = document.querySelector(".task-list");
 const button = document.getElementById("add-element");
 
+
+
 button.addEventListener("click", function() {
   let option = category.options[category.selectedIndex].value;
+  const list = document.querySelector(".task-list");
   const input = document.getElementById("input-task");
+  let li_elements = list.getElementsByTagName("li").length
 
 
-  var textnode = `<li class="item">
+  var textnode = `<div class="item">
   <p>${input.value}</p>
   <div class="remove" onclick="remove(this)">
   <i class="fa fa-trash"></i>
   </div>
-  </li>`;
+  </div>`;
 
   if (input.value.length > 0) {
+    // główna wersja 
     var node = document.createElement("LI");
-    node.innerHTML = textnode;
+    node.innerHTML = textnode
     document.getElementById(option).appendChild(node);
     input.placeholder = "Wpisz zadanie do  wykonania...";
     input.value = "";
+
+    console.log(li_elements)
 
     input.classList.remove("input-task--error");
   } else {
